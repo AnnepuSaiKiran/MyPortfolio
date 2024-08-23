8a0e718578af52d2f1960de2931acb1350ceefb4
AnnepuSaiKiran <99273804+AnnepuSaiKiran@users.noreply.github.com>
2024-08-22 21:11:01 -0500

    Update main.js

--- MyPortfolio/main.js
+++ MyPortfolio/main.js
@@ -1,7 +1,1 @@
-var typed = new Typed(".text",{
-    strings:[ "FrontEnd Developer","Java Developer", "Web Developer"],
-    typeSpeed : 10,
-    backSpeed : 100,
-    backDelay : 1000,
-    loop : true
-});
+
